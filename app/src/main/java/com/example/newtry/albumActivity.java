9e065a581f34a902172aaf3d2036ad7672ad2b1a
PeterGeorgee <petergeoorge@gmail.com>
2022-07-01 11:20:00 +0200

    [ADD] searching

--- app/src/main/java/com/example/newtry/albumActivity.java
+++ app/src/main/java/com/example/newtry/albumActivity.java
@@ -1,5 +1,6 @@
 package com.example.newtry;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -9,11 +10,16 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
 import com.squareup.picasso.Picasso;
 
@@ -28,7 +34,9 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class albumActivity extends AppCompatActivity {
 
@@ -41,6 +49,10 @@ public class albumActivity extends AppCompatActivity {
     int AlbID;
 
     List<String> photosURLS=new ArrayList<String>();
+
+    List<photoObj> myphotos=new ArrayList<photoObj>();
+
+    CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,13 +107,15 @@ public class albumActivity extends AppCompatActivity {
                 for(int i=0;i<jsonArray.length();i++){
                     JSONObject jsonObject=jsonArray.getJSONObject(i);
                     if(Integer.parseInt(jsonObject.getString("albumId"))==AlbID){
-                        photosURLS.add(jsonObject.getString("url"));
+                        photoObj photo=new photoObj(jsonObject.getString("title"),jsonObject.getString("url"));
+                        myphotos.add(photo);
+//                        photosURLS.add(jsonObject.getString("url"));
                     }
                 }
-                Log.e("my urls", String.valueOf(photosURLS));
+                Log.e("my urls", String.valueOf(myphotos.get(1).getUrl()));
 
 //                ArrayAdapter adapter=new ArrayAdapter(albumActivity.this, android.R.layout.simple_list_item_1,photosURLS);
-                CustomAdapter adapter=new CustomAdapter(albumActivity.this);
+                adapter=new CustomAdapter(albumActivity.this,myphotos);
                 gridView.setAdapter(adapter);
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -110,19 +124,59 @@ public class albumActivity extends AppCompatActivity {
         }
     }
 
-    public class CustomAdapter extends BaseAdapter{
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        MenuItem menuItem=menu.findItem(R.id.search_view);
+        SearchView searchView= (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return true;
+            }
+        });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+
+        if(id==R.id.search_view){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class CustomAdapter extends BaseAdapter implements Filterable {
 
         private Context context;
         private LayoutInflater layoutInflater;
 
-        public CustomAdapter(Context context) {
+        private List<photoObj> photosList;
+        private List<photoObj> photosListFiltered;
+
+
+        public CustomAdapter(Context context,List<photoObj> photosList) {
             this.context = context;
             this.layoutInflater=(LayoutInflater)context.getSystemService(LAYOUT_INFLATER_SERVICE);
+            this.photosList=photosList;
+            this.photosListFiltered=photosList;
+
         }
 
         @Override
         public int getCount() {
-            return photosURLS.size();
+            return photosListFiltered.size();
         }
 
         @Override
@@ -132,7 +186,7 @@ public class albumActivity extends AppCompatActivity {
 
         @Override
         public long getItemId(int i) {
-            return 0;
+            return i;
         }
 
         @Override
@@ -142,22 +196,48 @@ public class albumActivity extends AppCompatActivity {
             }
 
             ImageView imageView=view.findViewById(R.id.imgView);
-//            imageView.setImageURI(photosURLS.get(i));
-//            try {
-//                HttpURLConnection connection=(HttpURLConnection).openConnection();
-//                connection.setDoInput(true);
-//                connection.connect();
-//                InputStream input = connection.getInputStream();
-//                Bitmap myBitmap = BitmapFactory.decodeStream(input);
-//                imageView.setImageBitmap(myBitmap);
 
-                Picasso.get().load(photosURLS.get(i)).into(imageView);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
+            Log.e("i'm here",myphotos.get(i).getUrl());
+            Picasso.get().load(photosListFiltered.get(i).getUrl()).into(imageView);
 
 
             return view;
+        }
+
+        @Override
+        public Filter getFilter() {
+            Filter filter=new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence charSequence) {
+                    FilterResults filterResults=new FilterResults();
+
+                    if(charSequence==null || charSequence.length()==0){
+                        filterResults.count=photosList.size();
+                        filterResults.values=photosList;
+                    }else{
+                        String searchStr=charSequence.toString().toLowerCase();
+                        List<photoObj> resultData=new ArrayList<photoObj>();
+
+                        for (photoObj photo:photosList){
+                            if(photo.getTitle().contains(searchStr)){
+                                resultData.add(photo);
+                            }
+                            filterResults.count=resultData.size();
+                            filterResults.values=resultData;
+                        }
+
+                    }
+
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                    photosListFiltered= (List<photoObj>) filterResults.values;
+                    notifyDataSetChanged();
+                }
+            };
+            return filter;
         }
     }
 }
